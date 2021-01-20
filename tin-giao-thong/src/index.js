3e4d4520474de3429adc6b7c90987c76c67a00ca
lethiduyen000097 <duyenlt060097@gmail.com>
2021-01-20 13:44:21 +0700

    mapping collections to elements

--- tin-giao-thong/src/index.js
+++ tin-giao-thong/src/index.js
@@ -1,50 +1,52 @@
-
-// // Creating your own JSX elements
-// // 1. Encapsulating HTML
-
-// import React, { Component } from 'react';
-// import { render } from 'react-dom';
-
-// class MyComponent extends Component {
-//   render() {
-//     return (
-//       <section>
-//         <h1>My Component</h1>
-//         <p>Content in my component...</p>
-//       </section>
-//     );
-//   }
-// }
-
-
-// render(<MyComponent />, document.getElementById('root'));
-
-// // 2. Nested elements
+// // Using JavaScript expressions
 
 // import React from 'react';
 // import { render } from 'react-dom';
 
-// import MySection from './MySection';
-// import MyButton from './MyButton';
+// const enabled = false;
+// const text = "A Button";
+// const palceholder = 'input value...';
+// const size = 50;
 
 // render(
-//   <MySection>
-//     <MyButton>Học miễn phí</MyButton>
-//   </MySection>,
+//   <section>
+//     <button disabled={!enabled}>{text}</button>
+//     <input placeholder={palceholder} size={size} />
+//   </section>,
 //   document.getElementById('root')
 // );
 
-// 3. Namespaced components
 
+// // 
 import React from 'react';
 import { render } from 'react-dom';
 
-import MyComponent from './MyComponent';
+const array = ['Python', 'Java', 'C/C++', 'Javascript', 'Golang', 'R', 'Swift', 'PHP', 'C#', 'Matlab'];
+
+const object = {
+  first: 1,
+  second: 2,
+  third: 3
+};
 
 render(
-  <MyComponent>
-    <MyComponent.Luat />
-    <MyComponent.ThiBang />
-  </MyComponent>,
+  <section>
+    <h1>Các ngôn ngữ lập trình đáng học nhất năm 2020</h1>
+    <ul>
+      {array.map(i => (
+        <li key={i}>{i}</li>
+      ))}
+    </ul>
+
+    <h1>Lộ trình học lập trình React Native</h1>
+    <ul>
+      {Object.keys(object).map(i => (
+        <li key={i}>
+          <strong>{i}: </strong>
+          {/* {object[i]} */}
+        </li>
+      ))}
+    </ul>
+  </section>,
   document.getElementById('root')
 );
