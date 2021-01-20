07bb07c2bfe8244811f048ec648644b437e765ab
lethiduyen000097 <duyenlt060097@gmail.com>
2021-01-20 15:09:02 +0700

    merging the component state

--- tin-giao-thong/src/MyComponent.js
+++ tin-giao-thong/src/MyComponent.js
@@ -1,98 +1,30 @@
-// // import React, { Component } from 'react';
-
-// // class Luat extends Component {
-// //     render() {
-// //         return <p>Luật an toàn giao thông...</p>;
-// //     }
-// // }
-
-// // class ThiBang extends Component {
-// //     render() {
-// //         return <p>Thi bằng lái xe miễn phí...</p>;
-// //     }
-// // }
-
-// // class MyComponent extends Component {
-// //     render() {
-// //         return <section> {this.props.children}</section>;
-// //     }
-// // }
-
-// // MyComponent.Luat = Luat;
-// // MyComponent.ThiBang = ThiBang;
-
-// // export default MyComponent;
-
-// // export { Luat, ThiBang };
-
-
-// //chapter 3
-
-// // Setting an initial component state
-
-// import React, { Component } from 'react';
-
-// export default class MyComponent extends Component {
-//   state = {
-//     khoaCua: false,
-//     denPhongKhach: true,
-//     denNhaVeSinh: true,
-//     denNhaBep: true,
-//     denPhongNgu: true,
-//     mayGiat: true,
-//     tuoiHoa: false
-//   };
-
-//   render() {
-//     const { khoaCua, denPhongKhach, denNhaVeSinh, denNhaBep, denPhongNgu, mayGiat, tuoiHoa  } = this.state;
-
-//     return (
-//       <main>
-//         <h1>
-//             Hệ thống bật tắt các thiết bị điện trong nhà
-//         </h1>
-//         <section>
-//           <button disabled={khoaCua}>Khóa cửa</button>
-//         </section>
-//         <section>
-//           <button disabled={denPhongKhach}>Đèn phòng khách</button>
-//         </section>
-//         <section>
-//           <button disabled={denNhaVeSinh}>Đèn nhà vệ sinh</button>
-//         </section>
-//         <section>
-//           <button disabled={denNhaBep}>Đèn nhà bếp</button>
-//         </section>
-//         <section>
-//           <button disabled={denPhongNgu}>Đèn phòng ngủ</button>
-//         </section>
-//         <section>
-//           <button disabled={mayGiat}>Máy giặt</button>
-//         </section>
-//         <section>
-//           <button disabled={tuoiHoa}>Tưới hoa</button>
-//         </section>
-//       </main>
-//     );
-//   }
-// }
+// Merging the component state
 
 import React, { Component } from 'react';
 
 export default class MyComponent extends Component {
-  state = {
-    heading: 'React Awesomesauce (Busy) ',
-    content: 'Loading...'
-  };
+    state = {
+        first: 'loading...',
+        second: 'loading...',
+        third: 'loading...',
+        fourth: 'loading...',
+        doneMessage: 'finished!'
+    };
 
-  render() {
-    const { heading, content } = this.state;
+    render() {
+        const { state } = this;
 
-    return (
-      <main>
-        <h1>{heading}</h1>
-        <p>{content}</p>
-      </main>
-    );
-  }
+        return (
+            <ul>
+                {Object.keys(state)
+                .filter(key => key !== 'doneMessage')
+                .map(key => (
+                    <li key={key}>
+                        <strong>{key}: </strong>
+                        {state[key]}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
 }
--- tin-giao-thong/src/index.js
+++ tin-giao-thong/src/index.js
@@ -9,19 +9,47 @@
 // render(<MyComponent />, document.getElementById('root'));
 
 
-// Creating a component state
+// // Creating a component state
+
+// import React from 'react';
+// import { render } from 'react-dom';
+
+// import MyComponent from './MyComponent';
+
+// const myComponent = render(<MyComponent />,
+//   document.getElementById('root'));
+
+//   setTimeout(() => {
+//     myComponent.setState({
+//       heading: 'React Awesomesauce',
+//       content: 'Done!'
+//     });
+//   }, 3000);
+
 
 import React from 'react';
 import { render } from 'react-dom';
-
 import MyComponent from './MyComponent';
 
 const myComponent = render(<MyComponent />,
   document.getElementById('root'));
 
   setTimeout(() => {
-    myComponent.setState({
-      heading: 'React Awesomesauce',
-      content: 'Done!'
-    });
+    myComponent.setState({ first: 'done!' });
+  }, 1000);
+
+  setTimeout(() => {
+    myComponent.setState({ second: 'done!' });
+  }, 2000);
+
+  setTimeout(() => {
+    myComponent.setState({ third: 'done!' });
   }, 3000);
+
+  setTimeout(() => {
+    myComponent.setState(state => ({
+      ...state,
+      fourth: state.doneMessage
+    }));
+  }, 4000);
+  
