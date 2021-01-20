ac38657fce614f044375c0edb0f1218a0a01497a
lethiduyen000097 <duyenlt060097@gmail.com>
2021-01-20 15:56:07 +0700

    Setting property values

--- tin-giao-thong/src/MyButton.js
+++ tin-giao-thong/src/MyButton.js
@@ -1,10 +1,6 @@
+// Setting property values 
 import React, { Component } from 'react';
 export default class MyButton extends Component {
-    static defaultProps = {
-        disabled: false,
-        text: 'My Button'
-    };
-
     render() {
         const { disabled, text } = this.props;
 
--- tin-giao-thong/src/index.js
+++ tin-giao-thong/src/index.js
@@ -1,6 +1,30 @@
-// Default property values
 import React from 'react';
-import { render } from 'react-dom';
+import { render as renderJSX } from 'react-dom';
 import MyButton from './MyButton';
+import MyList from './MyList';
 
-render(<MyButton />, document.getElementById('root'));
+const appState = {
+  text: 'My Button',
+  disabled: true,
+  items: ['First', 'Second', 'Third']
+};
+
+function render(props) {
+  renderJSX(
+    <main>
+      <MyButton text={props.text} disabled={props.disabled} />
+      <MyList items={props.items} /> 
+    </main>,
+    document.getElementById('root')
+  );
+}
+
+render(appState);
+
+setTimeout(() => {
+  appState.disabled = false;
+  appState.items.push('Fourth');
+
+  render(appState);
+}, 1000);
+
