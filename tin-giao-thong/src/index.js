fd1f59025b134e9ed1eb8c728df5dc852a0f110d
lethiduyen000097 <duyenlt060097@gmail.com>
2021-01-20 14:35:49 +0700

    loading, 3000 s

--- tin-giao-thong/src/index.js
+++ tin-giao-thong/src/index.js
@@ -1,9 +1,27 @@
-//Chapter 3 Component Properties, State, and Context
+// //Chapter 3 Component Properties, State, and Context
 
-// Setting an initial component state
+// // Setting an initial component state
+
+// import React from 'react';
+// import { render } from 'react-dom';
+// import MyComponent from './MyComponent';
+
+// render(<MyComponent />, document.getElementById('root'));
+
+
+// Creating a component state
 
 import React from 'react';
 import { render } from 'react-dom';
+
 import MyComponent from './MyComponent';
 
-render(<MyComponent />, document.getElementById('root'));
+const myComponent = render(<MyComponent />,
+  document.getElementById('root'));
+
+  setTimeout(() => {
+    myComponent.setState({
+      heading: 'React Awesomesauce',
+      content: 'Done!'
+    });
+  }, 3000);
