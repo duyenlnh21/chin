48d1fc42fee99d6324f833c9bbf6327169fca884
lethiduyen000097 <duyenlt060097@gmail.com>
2021-01-20 13:56:34 +0700

    Fragments

--- tin-giao-thong/src/index.js
+++ tin-giao-thong/src/index.js
@@ -1,52 +1,15 @@
-// // Using JavaScript expressions
+// 1. Framents of JSX
 
-// import React from 'react';
-// import { render } from 'react-dom';
-
-// const enabled = false;
-// const text = "A Button";
-// const palceholder = 'input value...';
-// const size = 50;
-
-// render(
-//   <section>
-//     <button disabled={!enabled}>{text}</button>
-//     <input placeholder={palceholder} size={size} />
-//   </section>,
-//   document.getElementById('root')
-// );
-
-
-// // 
 import React from 'react';
 import { render } from 'react-dom';
 
-const array = ['Python', 'Java', 'C/C++', 'Javascript', 'Golang', 'R', 'Swift', 'PHP', 'C#', 'Matlab'];
+import WithoutFragments from './WithoutFragments';
+import WithFragments from './Fragments';
 
-const object = {
-  first: 1,
-  second: 2,
-  third: 3
-};
-
-render(
-  <section>
-    <h1>Các ngôn ngữ lập trình đáng học nhất năm 2020</h1>
-    <ul>
-      {array.map(i => (
-        <li key={i}>{i}</li>
-      ))}
-    </ul>
-
-    <h1>Lộ trình học lập trình React Native</h1>
-    <ul>
-      {Object.keys(object).map(i => (
-        <li key={i}>
-          <strong>{i}: </strong>
-          {/* {object[i]} */}
-        </li>
-      ))}
-    </ul>
-  </section>,
+render (
+  <div>
+    <WithoutFragments />
+    <WithFragments />
+  </div>,
   document.getElementById('root')
 );
