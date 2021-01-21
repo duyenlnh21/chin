99bda7e6eed24cb9b4c758128f3289271e72e322
lethiduyen000097 <duyenlt060097@gmail.com>
2021-01-21 11:57:58 +0700

    Canceling requests and resetting state

--- tin-giao-thong/src/App.js
+++ tin-giao-thong/src/App.js
@@ -1,52 +1,17 @@
-// import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState } from 'react';
+import User from './User';
 
-// export default function App() {
-//     const [name, setName] = useState('Adam');
-//     const [age, setAge] = useState(35);
-
-//     return (
-//         <Fragment>
-//             <section>
-//                 <input value={name} onChange={e => setName(e.target.value)} />
-//                 <p>My name is {name}</p>
-//             </section>
-//             <section>
-//                 <input 
-//                     type="number"
-//                     value={age}
-//                     onChange={e => setAge(e.target.value)}
-//                 />
-//                 <p>My age is {age}</p>
-//             </section>
-//         </Fragment>
-//     );
-// }
-
-import React, { Fragment, useEffect, useState } from 'react';
-function fetchUser() {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve({ id: 1, name: 'Adam' });
-        }, 1000);
-    });
-}
+const ShowHideUser = ({ show }) => (show ? <User /> : null);
 
 export default function App() {
-    const [id, setId] = useState('loading...');
-    const [name, setName] = useState('loading...');
-
-    useEffect(() => {
-        fetchUser().then(user => {
-            setId(user.id);
-            setName(user.name);
-        });
-    });
+    const [show, setShow] = useState(false);
 
     return (
         <Fragment>
-            <p> ID: {id}</p>
-            <p>Name: {name}</p>
+            <button onClick={() => setShow(!show)}>
+                {show ? 'Danh sách' : 'Xem chi tiet'}
+            </button>
+            <ShowHideUser show={show} />
         </Fragment>
     );
 }
-
