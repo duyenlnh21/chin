b3a2eca23434b08e345df4842f54697db7b6f696
lethiduyen000097 <duyenlt060097@gmail.com>
2021-01-21 11:05:29 +0700

    Fetching component data

--- tin-giao-thong/src/App.js
+++ tin-giao-thong/src/App.js
@@ -1,23 +1,52 @@
-import React, { Fragment, useState } from 'react';
+// import React, { Fragment, useState } from 'react';
+
+// export default function App() {
+//     const [name, setName] = useState('Adam');
+//     const [age, setAge] = useState(35);
+
+//     return (
+//         <Fragment>
+//             <section>
+//                 <input value={name} onChange={e => setName(e.target.value)} />
+//                 <p>My name is {name}</p>
+//             </section>
+//             <section>
+//                 <input 
+//                     type="number"
+//                     value={age}
+//                     onChange={e => setAge(e.target.value)}
+//                 />
+//                 <p>My age is {age}</p>
+//             </section>
+//         </Fragment>
+//     );
+// }
+
+import React, { Fragment, useEffect, useState } from 'react';
+function fetchUser() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve({ id: 1, name: 'Adam' });
+        }, 1000);
+    });
+}
 
 export default function App() {
-    const [name, setName] = useState('Adam');
-    const [age, setAge] = useState(35);
+    const [id, setId] = useState('loading...');
+    const [name, setName] = useState('loading...');
+
+    useEffect(() => {
+        fetchUser().then(user => {
+            setId(user.id);
+            setName(user.name);
+        });
+    });
 
     return (
         <Fragment>
-            <section>
-                <input value={name} onChange={e => setName(e.target.value)} />
-                <p>My name is {name}</p>
-            </section>
-            <section>
-                <input 
-                    type="number"
-                    value={age}
-                    onChange={e => setAge(e.target.value)}
-                />
-                <p>My age is {age}</p>
-            </section>
+            <p> ID: {id}</p>
+            <p>Name: {name}</p>
         </Fragment>
     );
 }
+
